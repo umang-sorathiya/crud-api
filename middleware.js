db41ce0fc3dd0e10e7186dc146ff5ef21ee22158
umang <over.umang88@gmail.com>
2026-05-23 09:34:33 +0530

    first commit

---
+++ middleware.js
@@ -0,0 +1,22 @@
+const jwt = require('jsonwebtoken')
+require('dotenv').config()
+
+function checkToken(req, res, next) {
+  const authHeader = req.headers.authorization
+
+  if (!authHeader) {
+    return res.status(401).send({ error: 'No token provided' })
+  }
+
+  const token = authHeader.split(' ')[1]
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = decoded
+    next()
+  } catch {
+    return res.status(401).send({ error: 'Invalid token' })
+  }
+}
+
+module.exports = checkToken
